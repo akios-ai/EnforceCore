053006e46b1d6ddaad8d50f0b730fc33f7b71f4e
aj <aj@akios.ai>
2026-02-25 10:56:18 +0100

    release: v1.1.3

--- enforcecore/telemetry/instrumentor.py
+++ enforcecore/telemetry/instrumentor.py
@@ -33,7 +33,7 @@ from enforcecore.telemetry.metrics import EnforceCoreMetrics
 logger = structlog.get_logger("enforcecore.telemetry")
 
 _SCOPE_NAME = "enforcecore"
-_SCOPE_VERSION = "1.1.2"
+_SCOPE_VERSION = "1.1.3"
 
 
 class EnforceCoreInstrumentor:
